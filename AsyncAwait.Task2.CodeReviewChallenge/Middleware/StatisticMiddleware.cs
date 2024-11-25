aad7181f8d81bb99f46a762e4f61d042ea09108c
Akhilesh2690 <akhilesh_kumar26@outlook.com>
2024-11-25 11:07:52 +0530

    statistic middleware code changes

--- AsyncAwait.Task2.CodeReviewChallenge/Middleware/StatisticMiddleware.cs
+++ AsyncAwait.Task2.CodeReviewChallenge/Middleware/StatisticMiddleware.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
-using AsyncAwait.Task2.CodeReviewChallenge.Headers;
+﻿using AsyncAwait.Task2.CodeReviewChallenge.Headers;
 using CloudServices.Interfaces;
 using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
 
 namespace AsyncAwait.Task2.CodeReviewChallenge.Middleware;
 
@@ -23,20 +22,32 @@ public class StatisticMiddleware
     {
         string path = context.Request.Path;
 
-        var staticRegTask = Task.Run(
-            () => _statisticService.RegisterVisitAsync(path)
-                .ConfigureAwait(false)
-                .GetAwaiter().OnCompleted(UpdateHeaders));
-        Console.WriteLine(staticRegTask.Status); // just for debugging purposes
+        //var staticRegTask = Task.Run(
+        //    () => _statisticService.RegisterVisitAsync(path)
+        //        .ConfigureAwait(false)
+        //        .GetAwaiter().OnCompleted(UpdateHeaders));
+        //Console.WriteLine(staticRegTask.Status); // just for debugging purposes
 
-        void UpdateHeaders()
-        {
-            context.Response.Headers.Add(
-                CustomHttpHeaders.TotalPageVisits,
-                _statisticService.GetVisitsCountAsync(path).GetAwaiter().GetResult().ToString());
-        }
+        //Task create new  a thread  async on threadpool but async methods are already designed to run asynchronously without blocking a thread.
 
-        Thread.Sleep(3000); // without this the statistic counter does not work
+        //void UpdateHeaders()
+        //{
+        //    context.Response.Headers.Add(
+        //        CustomHttpHeaders.TotalPageVisits,
+        //        _statisticService.GetVisitsCountAsync(path).GetAwaiter().GetResult().ToString()); //GetVisitsCountAsync should be await as,it blocks the execution of the thread until the result is returned.
+        //}
+
+        //Thread.Sleep(3000); // without this the statistic counter does not work ,(It blocks the thread ,wait for 3 sec.)
+
+        //await _next(context);
+
+        await _statisticService.RegisterVisitAsync(path);
+        var visitsCount = await _statisticService.GetVisitsCountAsync(path);
+
+        context.Response.Headers.Add(
+            CustomHttpHeaders.TotalPageVisits,
+            visitsCount.ToString());
+
         await _next(context);
     }
 }
